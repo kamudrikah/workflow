84eb8809ee2f3a55daa6344dd0dca43dc7df403c
fakhruz <fakhruz.amtis@gmail.com>
2013-12-17 09:27:35 +0800

    commit 0.2

---
+++ application/controllers/function2.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Function2 extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('function2');
+	}
+}
+
+/* End of file main.php */
+/* Location: ./application/controllers/main.php */
