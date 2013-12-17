43907114da852b7c4a09a6cd43a0b07fca20d550
fakhruz <fakhruz.amtis@gmail.com>
2013-12-17 09:43:22 +0800

    0.3

--- application/views/main.php
+++ application/views/main.php
@@ -2,5 +2,5 @@ Workflow Main Page
 <ul>
 	<li><a href="<?= site_url();?>/function1">0.1</a></li>
 	<li><a href="<?= site_url();?>/function2">0.2</a></li>
-	<li><a href="">asd</a></li>
+	<li><a href="">0.3</a></li>
 </ul>
