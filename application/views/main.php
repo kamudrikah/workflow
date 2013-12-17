a70732431d955fd1f8fecbecea55cced01e9150e
Ku Ahmad Mudrikah <kamudrikah@gmail.com>
2013-12-17 09:44:34 +0800

    0.4

--- application/views/main.php
+++ application/views/main.php
@@ -3,4 +3,5 @@ Workflow Main Page
 	<li><a href="<?= site_url();?>/function1">0.1</a></li>
 	<li><a href="<?= site_url();?>/function2">0.2</a></li>
 	<li><a href="">0.3</a></li>
+	<li><a href="">0.4</a></li>
 </ul>
